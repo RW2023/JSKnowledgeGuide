deb46e49a91399e70f247eed3d56d2e3af671f91
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-02-17 09:02:27 -0500

    create .js's, css and reorder links

--- src/scripts/hl.js
+++ src/scripts/hl.js
@@ -1,4 +1,10 @@
-import hljs from "highlight.js";
+// import hljs from "highlight.js";
 
 // Call the highlightAll() function to apply syntax highlighting to all code blocks on the page
-hljs.highlightAll();
+// hljs.highlightAll();
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelectorAll("pre code").forEach((el) => {
+    hljs.highlightElement(el);
+  });
+});
